f0a6216c33ed224d5b5847651d47e1913a975a49
butterfly1of4 <jhouck29@gmail.com>
2020-08-04 09:04:10 -0400

    updating

---
+++ frontend_art_api/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';
+import { Route, Link, Redirect } from "react-router-dom";
+
+const hostURL = "https://rocky-hamlet-98173.herokuapp.com/record";
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			entry: [],
+    };
+    this.setState = this.setState.bind(this);
+
+		console.log(this.state.entry);
+	}
+	setData(entry) {
+		this.setState({ entry: entry });
+	}
+  
+
+  render() {
+      return(<div>
+        <nav>
+					<Link to='/'>
+						<h1>Harvard Art API</h1>
+					</Link>
+				</nav>
+				<main>
+
+      
+				</main>
+        </div>
+      )}
+      componentDidMount() {
+        fetch(hostURL, getAll, deleteOne, getOne, put, post)
+          .then((res) => res.json())
+          .then((res) => {
+            console.log(res);
+    
+            let entry = res;
+            console.log(entry);
+            this.setState({
+              entry: res,
+            });
+            console.log(entry);
+            console.log(this.state.entry);
+            return this.state.entry;
+          });
+      }
+      componentDidUpdate() {
+        console.log("the end");
+        console.log(this.state.entry);
+      }
+}
+export default App;
