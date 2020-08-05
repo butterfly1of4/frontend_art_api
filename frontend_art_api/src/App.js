b84e7ef5f4079edf9fc4438e52e78b12210c650d
butterfly1of4 <jhouck29@gmail.com>
2020-08-05 13:16:01 -0400

    adding methods

--- frontend_art_api/src/App.js
+++ frontend_art_api/src/App.js
@@ -1,58 +1,91 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import { Route, Link, Redirect } from "react-router-dom";
 
 const hostURL = "https://rocky-hamlet-98173.herokuapp.com/record";
 
+const getAll = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+// const getOne = {
+// 	method: "GET",
+// 	headers: {
+// 		"Content-Type": "application/json",
+// 	},
+// };
+const put = {
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+const post = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+const deleteOne = {
+  method: "DELETE",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			entry: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      entry: [],
     };
     this.setState = this.setState.bind(this);
 
-		console.log(this.state.entry);
-	}
-	setData(entry) {
-		this.setState({ entry: entry });
-	}
-  
+    console.log(this.state.entry);
+  }
+  setData(entry) {
+    this.setState({ entry: entry });
+  }
 
   render() {
-      return(<div>
+    return (
+      <div>
         <nav>
-					<Link to='/'>
-						<h1>Harvard Art API</h1>
-					</Link>
-				</nav>
-				<main>
+          <Link to="/">
+            <h1>Harvard Art API</h1>
+          </Link>
+        </nav>
+        <main>
+          <div></div>
+        </main>
+      </div>
+    ); //return
+  } //render
 
-      
-				</main>
-        </div>
-      )}
-      componentDidMount() {
-        fetch(hostURL
-          // , getAll, deleteOne, getOne, put, post
-          )
-          .then((res) => res.json())
-          .then((res) => {
-            console.log(res);
-    
-            let entry = res;
-            console.log(entry);
-            this.setState({
-              entry: res,
-            });
-            console.log(entry);
-            console.log(this.state.entry);
-            return this.state.entry;
-          });
-      }
-      componentDidUpdate() {
-        console.log("the end");
+  componentDidMount() {
+    fetch(
+      hostURL
+      // , getAll, deleteOne, getOne, put, post
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+
+        let entry = res;
+        console.log(entry);
+        this.setState({
+          entry: res,
+        });
+        console.log(entry);
         console.log(this.state.entry);
-      }
+        return this.state.entry;
+      });
+  }
+  componentDidUpdate() {
+    console.log("the end");
+    console.log(this.state.entry);
+  }
 }
 export default App;
---
+++ frontend_art_api/src/Components/delete.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+
+//api host url import?
+
+const deleteOne = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  class Delete extends Component {
+      constructor(){
+          super()
+          this.state = {
+              entry: [],
+              onClick: true
+          }
+      }//constructor
+
+      componentWillMount() {
+        fetch(
+          hostURL, deleteOne
+        ).then((res) => {
+            res.json()
+        }).then((entry) => this.setState({entry}))
+        .catch((err) => {
+            console.log(err)
+        })
+     
+        } //componentWillMount
+  }
