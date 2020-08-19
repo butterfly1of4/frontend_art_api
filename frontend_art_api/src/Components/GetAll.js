98cc82bf55085ddbbf7bd93c1acde042c423d7a1
butterfly1of4 <jhouck29@gmail.com>
2020-08-19 13:09:51 -0400

    added Get

--- frontend_art_api/src/Components/GetAll.js
+++ frontend_art_api/src/Components/GetAll.js
@@ -2,3 +2,44 @@ import React, { Component } from "react";
 import "./App.css";
 import { Route, Link, Redirect } from "react-router-dom";
 import { Link } from "react-router-dom";
+
+const hostURL = "https://rocky-hamlet-98173.herokuapp.com/record";
+
+const getOne = {
+	method: "GET",
+	headers: {
+		"Content-Type": "application/json",
+	},
+};
+
+class GetAll extends Component{
+    constructor() {
+        super()
+        this.state={
+            picture: []
+        }
+    }
+    componentWillMount(){
+        fetch(hostURL, GetAll)
+        .then((res) => res.json())
+        .then((picture) => this.setState({picture}))
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+    render() {
+        let list = this.state.picture.map((item) => {
+            return (
+                <div className='container' key={item}>
+                    <img src= {item.image} />
+                </div>
+            )
+        })
+        return (
+            <React.Fragment>
+                <div className="list">{list}</div>
+            </React.Fragment>
+        )
+    } 
+}
+export default GetAll
